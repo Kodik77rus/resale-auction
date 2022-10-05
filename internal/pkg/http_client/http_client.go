3f4bf021be28797d7dfeaff1e942fc5555b67e41
Kodik77rus <plyhach@gmail.com>
2022-10-05 23:04:15 +0300

    init http client

---
+++ internal/pkg/http_client/http_client.go
@@ -0,0 +1,58 @@
+package http_client
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type HttpClient struct {
+	client http.Client
+}
+
+func InitHttpClient() *HttpClient {
+	return &HttpClient{
+		client: http.Client{},
+	}
+}
+
+func (h HttpClient) POST(
+	rawURL string,
+	data []byte,
+	headers http.Header,
+	timeout time.Duration,
+) (int, []byte, error) {
+	fetchUrl, err := url.Parse(rawURL)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	httpReq := &http.Request{
+		Method: http.MethodPost,
+		URL:    fetchUrl,
+		Header: headers,
+	}
+
+	return h.sendReq(httpReq, timeout)
+}
+
+func (h *HttpClient) sendReq(
+	req *http.Request,
+	timeout time.Duration,
+) (int, []byte, error) {
+	h.client.Timeout = timeout
+
+	resp, err := h.client.Do(req)
+	if err != nil {
+		return 0, nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return resp.StatusCode, body, nil
+}
