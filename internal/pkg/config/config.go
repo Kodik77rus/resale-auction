454dd1b003c31502b11c30adc86bf0aad9f85671
Kodik77rus <plyhach@gmail.com>
2022-10-05 23:50:02 +0300

    add dsp  & spp  request timeouts

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"strings"
+	"time"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/pkg/errors"
@@ -12,12 +13,16 @@ type Config struct {
 	PORT                string
 	DSP_CONNECTION_URLS []string
 	LOG_LVL             string
+	SSP_TIMEOUT         time.Duration
+	DSP_TIMEOUT         time.Duration
 }
 
 func InitConfig() (*Config, error) {
 	port := flag.String("p", "8080", "server port")
 	dspUrls := flag.String("d", "", "dsp url")
 	logLVL := flag.String("l", "debug", "log lvl")
+	dspTimeout := flag.Duration("dt", 200, "dsp request timeout")
+	sspTimeout := flag.Duration("st", 250, "ssp request timeout")
 
 	flag.Parse()
 
@@ -37,5 +42,7 @@ func InitConfig() (*Config, error) {
 		PORT:                *port,
 		DSP_CONNECTION_URLS: dspSlice,
 		LOG_LVL:             *logLVL,
+		SSP_TIMEOUT:         *sspTimeout * time.Millisecond,
+		DSP_TIMEOUT:         *dspTimeout * time.Millisecond,
 	}, nil
 }
